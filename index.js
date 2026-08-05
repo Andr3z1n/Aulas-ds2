97d03151feea1018917016efd5ff793f92b44955
Andre Ryan <andre.soares12@aluno.ce.gov.br>
2026-08-05 08:45:55 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+// EXEMPLO 1
+
+//const aluno = {
+//      nome: "John",
+//    idade: 17,
+//    curso: "Desenvolvimento de Sistemas"
+//}
+
+//console.log(aluno)
+//console.log(aluno.nome)
+//console.log(aluno.idade)
+//console.log(aluno.curso)
+
+
+// EXEMPLO 2
+
+//const cachorro1 = {
+//    nome: "Rex",
+//
+//    latir() {
+//        return "Au au!"
+//    }
+//}
+
+//const cachorro2 = {
+//    nome: "Hulk",
+
+//    latir() {
+//        return `Woof Woof!!`
+//    }
+//}
+
+// EXEMPLO 3 - Classe
+
+//class Aluno {
+//    constructor(nome, idade, curso) {
+//       this.nome = nome
+//       this.idade = idade
+//       this.curso = curso
+//    }
+//}
+
+// Instâncias Ex1
+//const Andre = new Aluno("Andre", 16, "Lavagem Automotiva")
+//const Delaila = new Aluno("Delaila", 6, "Estudante")
+//const Daniara = new Aluno("Daniara", 37, "Dona de casa")
+//const Edicarlos = new Aluno("Edicarlos", 35, "Mecânico")
+//const Pedro = new Aluno("Pedro", 14, "Adm")
+
+// Execução
+//console.log(Andre.nome)
+//console.log(Delaila.nome)
+//console.log(Daniara.nome)
+//console.log(Edicarlos.nome)
+//console.log(Pedro.nome)
+
+//Ex2
+//class Produto {
+//    constructor(nome, preco) {
+//        this.nome = nome
+//        this.preco = preco
+//    }
+//}
+
+//const SacoDeGelo = new Produto("SacoDeGelo", 7.00)
+//const Pipoca = new Produto("Pipoca", 2.00)
+
+//console.log(SacoDeGelo)
+//console.log(Pipoca)
